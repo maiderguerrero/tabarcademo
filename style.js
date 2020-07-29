4b9091736e51330fa29a4fec85b5434a92aaac8b
maiderguerrero <47318536+maiderguerrero@users.noreply.github.com>
2020-07-29 14:04:16 +0200

    Update style.js

--- style.js
+++ style.js
@@ -29,12 +29,6 @@
 			
 				{ name: 'Celda 1', element: 'td', attributes: { 'class': 'bck-td-1' } },
 				{ name: 'Celda 2', element: 'td', attributes: { 'class': 'bck-td-2' } },
-				{ name: 'Celda 3', element: 'td', attributes: { 'class': 'bck-td-3' } },
-				{ name: 'Celda 4', element: 'td', attributes: { 'class': 'bck-td-4' } },
-				{ name: 'Celda 5', element: 'td', attributes: { 'class': 'bck-td-5' } },
-				{ name: 'Celda 6', element: 'td', attributes: { 'class': 'bck-td-6' } },
-				{ name: 'Celda 7', element: 'td', attributes: { 'class': 'bck-td-7' } },
-				{ name: 'Celda 8', element: 'td', attributes: { 'class': 'bck-td-8' } },
 			
 				{ name: 'Icono Escucha', element: 'span', attributes: { 'class': 'icon icon-escucha' } },
 				
